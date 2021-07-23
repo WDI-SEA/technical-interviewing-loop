a239390628260308685d94950d2795b742398053
TERRYSZHOU <TERRYSZHOU@GMAIL.COM>
2021-07-23 16:32:53 -0500

    Finished whiteboarding pseudo.py.

---
+++ whiteboarding/pseudo.py
@@ -0,0 +1,12 @@
+# define function(string1, string2)
+    # declare count variable
+
+    # iterate through string1
+        # iterate through string2
+            #if match, continue
+            #elif no match, count += 1
+    # return count
+
+
+
+# print function(string1, string2)
