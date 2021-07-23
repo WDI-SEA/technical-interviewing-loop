a0c2274845867aada66f57c8c1e71d135949bdaa
TERRYSZHOU <TERRYSZHOU@GMAIL.COM>
2021-07-23 16:09:09 -0500

    Installed axios and nodemon, created .gitignore for modules, challenge-2 solve

---
+++ coding-challenge/script.js
@@ -0,0 +1,22 @@
+const axios = require('axios')
+
+const findRando = () => {
+    randoArray = []
+
+    axios.get("https://randomuser.me/api/?results=10")
+        .then(res => {
+            let results = res.data.results
+            for (i = 0; i < results.length; i++) {
+                randoArray[i] = {
+                    name: `${results[i].name.title} ${results[i].name.first} ${results[i].name.last}`,
+                    picture: results[i].picture.large
+                }
+            }
+            console.log(randoArray)
+        }) 
+        .catch(err => console.log(err))
+
+}
+
+findRando()
+    
